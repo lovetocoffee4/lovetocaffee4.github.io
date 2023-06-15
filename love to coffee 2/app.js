3fab3fba3303c0c11d31e1ee0d36033254f371ae
lovetocoffee4 <136665241+lovetocoffee4@users.noreply.github.com>
2023-06-14 20:45:21 -0600

    Love To Coffee

---
+++ love to coffee 2/app.js
@@ -0,0 +1,14 @@
+const btnAbrirModal =
+document.querySelector("#btn-abrir-modal");
+const btnCerrarModal =
+document.querySelector("#btn-cerrar-modal");
+const modal =
+document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click",()=>{
+    modal.show();
+});
+
+btnCerrarModal.addEventListener("click",()=>{
+    modal.close();
+});
